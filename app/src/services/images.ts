90cf2d82e7fa36b8fbc12ba7fa5677db4af0fe07
ouwargui <guikemon2@gmail.com>
2021-08-30 20:36:05 -0300

    adds some images to use later

---
+++ app/src/services/images.ts
@@ -0,0 +1,6 @@
+const images = [
+  'https://images.wallpaperscraft.com/image/sneakers_shoes_style_173533_1024x768.jpg',
+  'https://i1.wp.com/pitimport.com/wp-content/uploads/2020/06/Banner-utraboost-bape3.png?fit=1024%2C424&ssl=1',
+];
+
+export default images;
